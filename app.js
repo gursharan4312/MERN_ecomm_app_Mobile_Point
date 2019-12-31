39155790bbf07b1a5638a7cc06a3cbab67822a3a
Gursharan Singh <gursharansingh4312@gmail.com>
2019-12-31 02:37:43 -0800

    backend initilized

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./client/build"));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
